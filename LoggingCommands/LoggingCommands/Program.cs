4cd2f115e567926a21c7e47b0c511d0ed9c59a09
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2025-10-30 09:16:27 +0800

    Test variables

---
+++ LoggingCommands/LoggingCommands/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LoggingCommands
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("##vso[task.setvariable variable=CWResult]HelloWorld!");
+        }
+    }
+}
