5b6cd0aca787c9d45bc1fe5ae50e3b5c97648ff4
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2025-12-05 13:40:22 +0800

    Test

---
+++ DotnetFramework472/DotnetFramework472/LogFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetFramework472
+{
+    public class LogFile
+    {
+        public static void LogMessage(object msg, LogType logType = LogType.Information)
+        {
+            var logissue = "";
+            switch (logType)
+            {
+                case LogType.Error:
+                    logissue = "##vso[task.logissue type=error]";
+                    break;
+                case LogType.Warning:
+                    logissue = "##vso[task.logissue type=warning]";
+                    break;
+                default:
+                    logissue = "";
+                    break;
+            }
+            Console.WriteLine($"{logissue}{msg}");
+        }
+
+        public enum LogType
+        {
+            Information,
+            Warning,
+            Error
+        }
+    }
+}
---
+++ DotnetFramework472/DotnetFramework472/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetFramework472
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LogFile.LogMessage("Information");
+            LogFile.LogMessage("Warning", LogFile.LogType.Warning);
+            LogFile.LogMessage("Error", LogFile.LogType.Error);
+        }
+    }
+}
