6b19fac99ed2f3f996d68e7ff180289d5c1807b1
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2025-12-19 10:02:38 +0800

    Check which files are needed

---
+++ Testing/Testing/Program.cs
@@ -0,0 +1,13 @@
+﻿using VSEng.Mailer;
+namespace Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var message = VSEngMailer.CreateMailMessage("v-wexu@microsoft.com", "Test", "This is a test email.");
+            VSEngMailer.SendMail(message);
+        }
+    }
+}
