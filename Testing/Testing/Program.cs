208fe53673d920d656eb3c9355ec3c956c064a0f
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2025-12-22 09:28:47 +0800

    Test

--- Testing/Testing/Program.cs
+++ Testing/Testing/Program.cs
@@ -4,7 +4,7 @@ namespace Testing
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             // Console.WriteLine("Hello, World!");
             // var message = VSEngMailer.CreateMailMessage("v-wexu@microsoft.com", "Test", "This is a test email.");
@@ -12,10 +12,11 @@ namespace Testing
             try
             {
                 Console.WriteLine("##vso[task.complete result=partiallySucceeded;]WARNING! The process ran into an error at some point.");
+                return 0;
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"##vso[task.logissue type=error]{ex.Message}");
+                return 1;
             }
         }
     }
