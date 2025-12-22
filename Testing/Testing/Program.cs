e600eb15ba4608517982f6cb313b2a77c78a274c
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2025-12-22 09:24:35 +0800

    Test

--- Testing/Testing/Program.cs
+++ Testing/Testing/Program.cs
@@ -1,13 +1,15 @@
-﻿using VSEng.Mailer;
+﻿// using VSEng.Mailer;
+using System;
 namespace Testing
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            var message = VSEngMailer.CreateMailMessage("v-wexu@microsoft.com", "Test", "This is a test email.");
-            VSEngMailer.SendMail(message);
+            // Console.WriteLine("Hello, World!");
+            // var message = VSEngMailer.CreateMailMessage("v-wexu@microsoft.com", "Test", "This is a test email.");
+            // VSEngMailer.SendMail(message);
+            Console.WriteLine("##vso[task.complete result=partiallySucceeded;]WARNING! The process ran into an error at some point.");
         }
     }
 }
