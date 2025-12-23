a9eb36bbf563d4abc8026b17107a4861917556d0
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2025-12-23 10:10:02 +0800

    Test

---
+++ LogFile.psm1
@@ -0,0 +1,27 @@
+Write-Host "Loading module LogFile"
+
+enum LogType {
+    Information
+    Warning
+    Error
+}
+
+Function Write-LogMessage
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] $Message,
+        [Parameter(Mandatory=$false)]
+        [LogType] $LogType = [LogType]::Information
+    )
+
+    switch ($LogType)
+    {
+        [LogType]::Warning     { Write-Warning "$Message" }
+        [LogType]::Error       { Write-Error("$Message") }
+        [LogType]::Information { Write-Host "Information: $Message" }
+    }
+}
+
+Export-ModuleMember -Function Write-LogMessage
