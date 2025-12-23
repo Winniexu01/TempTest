a3c557b4a3c929bf9b7c91a831f1a3c60fa053c3
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2025-12-23 18:25:54 +0800

    Test

---
+++ MultipleProj/MultipleProj/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ProjB;
+namespace MultipleProj
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Output.OutputMessage();
+        }
+    }
+}
---
+++ MultipleProj/ProjB/Output.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjB
+{
+    public static class Output
+    {
+        public static void OutputMessage()
+        {
+            Console.WriteLine("This is ProjB Output");
+        }
+    }
+}
