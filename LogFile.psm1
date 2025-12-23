17c0a2b2640de7742068fa7aba9c8f8eff4c5a4f
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2025-12-23 10:53:35 +0800

    Test

--- LogFile.psm1
+++ LogFile.psm1
@@ -19,7 +19,7 @@ Function Write-LogMessage
     switch ($LogType)
     {
         ([LogType]::Warning)     { Write-Warning "$Message" }
-        ([LogType]::Error)       { Write-Error "$Message" }
+        ([LogType]::Error)       { $Host.UI.WriteErrorLine("$Message") }
         ([LogType]::Information) { Write-Host "Information: $Message" }
     }
 }
