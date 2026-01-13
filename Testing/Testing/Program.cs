f7e9f6c1de9e5f60ea0b504009efc802ab8e8004
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2026-01-13 16:17:23 +0800

    TTTTE

--- Testing/Testing/Program.cs
+++ Testing/Testing/Program.cs
@@ -6,31 +6,7 @@ namespace Testing
     {
         static int Main(string[] args)
         {
-            string collectionUri = Environment.GetEnvironmentVariable("SYSTEM_TEAMFOUNDATIONCOLLECTIONURI");
-            string teamProject = Environment.GetEnvironmentVariable("SYSTEM_TEAMPROJECT");
-            string buildId = Environment.GetEnvironmentVariable("BUILD_BUILDID"); // Use BUILD_BUILDID for build pipelines
-
-            // Construct the full URL
-            if (!string.IsNullOrEmpty(collectionUri) && !string.IsNullOrEmpty(teamProject) && !string.IsNullOrEmpty(buildId))
-            {
-                // Example format for a build pipeline URL
-                string pipelineUrl = $"{collectionUri}{teamProject}/_build/results?buildId={buildId}";
-                Logger.LogMessage($"Current Pipeline URL: {pipelineUrl}");
-            }
-            else
-            {
-                Logger.LogMessage("Could not find all required Azure Pipelines environment variables.", LogType.Error);
-            }
-
-            Logger.LogMessage("Starting the process...");
-            Logger.LogMessage("");// blank line for readability
-            Logger.LogMessage("This is a test log message.", LogType.Warning);
-            Logger.LogMessage("");// blank line for readability
-            Logger.LogMessage("This is a test log message.", LogType.Error);
-            if (ErrorManager.HasError)
-                Console.WriteLine("##vso[task.complete result=SucceededWithIssues;]WARNING! The process ran into an error at some point.");
-            Logger.LogMessage("");// blank line for readability
-            Logger.LogMessage("Process finished.");
+            System.Console.WriteLine(Environment.GetEnvironmentVariable("SYSTEM_DEFINITIONNAME"));
             return 0;
         }
     }
