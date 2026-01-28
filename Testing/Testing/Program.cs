e46198e7d786e378593ae75ef994df69045b89fc
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2026-01-28 10:48:48 +0800

    Test

--- Testing/Testing/Program.cs
+++ Testing/Testing/Program.cs
@@ -6,7 +6,22 @@ namespace Testing
     {
         static int Main(string[] args)
         {
-            System.Console.WriteLine(Environment.GetEnvironmentVariable("SYSTEM_DEFINITIONNAME"));
+            var isDebugMode = Environment.GetEnvironmentVariable("DEBUG_MODE");
+            if (!string.IsNullOrEmpty(isDebugMode) && isDebugMode.Equals("true", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("DEBUG_MODE is enabled...");
+
+                var prWorkItems = Environment.GetEnvironmentVariable("PRWorkItems") ?? "";
+                var scheduledWorkItems = Environment.GetEnvironmentVariable("ScheduledWorkItems") ?? "";
+                var noAutoWorkItems = Environment.GetEnvironmentVariable("NoAutoWorkItems") ?? "";
+                Console.WriteLine($"PR Work Items: {prWorkItems}");
+                Console.WriteLine($"Scheduled Work Items: {scheduledWorkItems}");
+                Console.WriteLine($"No Auto Work Items: {noAutoWorkItems}");
+            }
+            else
+            {
+                Console.WriteLine("DEBUG_MODE is not enabled.");
+            }
             return 0;
         }
     }
