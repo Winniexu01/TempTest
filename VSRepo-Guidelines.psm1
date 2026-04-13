9ccac649267af1cc0d0e9dacbcce5bbff9491d14
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2026-04-13 12:27:22 +0800

    Test winget command

--- VSRepo-Guidelines.psm1
+++ VSRepo-Guidelines.psm1
@@ -2,10 +2,61 @@ write-host "Loading module VSRepo-Guidelines"
 $scriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 Import-Module -Name "$scriptDir\LogFile.psm1" -Force
 
-Function Test-Module {
-    Write-Host "Script directory: $scriptDir"
-    Write-Host "Working directory: $(Get-Location)"
-    Write-Host "System default working directory: $env:SYSTEM_DEFAULTWORKINGDIRECTORY"
+
+Function SendReportEmail {
+    param (
+        [Parameter(Mandatory=$true)]
+        [psobject] $email,
+        [Parameter(Mandatory=$false)]
+        [string] $method = '1'
+    )
+
+    # Force TLS1.2 when sending e-mail
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    try {
+        $dlls = @(
+            "System.Diagnostics.DiagnosticSource.dll",
+            "VSEng.Mailer.dll"
+        )
+        if ($method -eq '2') {
+            Write-LogMessage -Message "Loading mailer assembly with method 2 (from script directory)"
+            (Get-ChildItem -Path "$scriptDir\VSEng.Mailer" -Recurse -File -Include $dlls -ErrorAction SilentlyContinue | Where-Object { $_.FullName -like '*\net472\*' }).ForEach{
+                Write-LogMessage -Message "Loading assembly: $($_.FullName)"
+                [System.Reflection.Assembly]::LoadFrom($_.FullName)
+            }
+        }
+        else {
+            Write-LogMessage -Message "Loading mailer assembly with method 1 (from current directory)"
+            (Get-ChildItem -Path $PWD.Path -Recurse -File -Include $dlls -ErrorAction SilentlyContinue).ForEach{
+                Write-LogMessage -Message "Loading assembly: $($_.FullName)"
+                [System.Reflection.Assembly]::LoadFrom($_.FullName)
+            }
+        }
+
+        $mail_obj = [VSEng.Mailer.VSEngMailer]::CreateMailMessage($email.To, $email.Cc, "", $from, $email.Subject, $email.Body)
+        [VSEng.Mailer.VSEngMailer]::SendMail($mail_obj)
+    }
+    catch {
+        # we can't send an error email if email sending fails, so instead append to the log file.
+        Write-LogMessage -Message "SendReportEmail: Exception type: $($_.Exception.GetType().FullName)" -LogType 'Error'
+        Write-LogMessage -Message "Exception message: $($_.Exception.Message)" -LogType 'Error'
+        Write-LogMessage -Message "Full exception: $($_.Exception.ToString())" -LogType 'Error'
+    }
 }
 
-Export-ModuleMember -Function Test-Module
+Function Test-Function() {
+    Param (
+        [Parameter(Mandatory=$false)]
+        [string] $method = '2'
+    )
+    $body = "This is a test email to verify that the email sending function works correctly.<br><br>"
+    $email = New-Object PSObject
+    Add-Member -inputobject $email -membertype noteproperty -name To -value "v-wexu@microsoft.com"
+    Add-Member -inputobject $email -membertype noteproperty -name Cc -value ""
+    Add-Member -inputobject $email -membertype noteproperty -name Subject -Value "Test Email from PowerShell"
+    Add-Member -inputobject $email -membertype noteproperty -name Body -Value $body
+    SendReportEmail -email $email -method $method
+}
+
+Export-ModuleMember -Function Test-Function
