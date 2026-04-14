fddef16829b4fe74f91c43209f4e693759efdac4
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2026-04-14 12:05:09 +0800

    test

--- src/Test.psm1
+++ src/Test.psm1
@@ -1,15 +1,16 @@
 function Test-Function() {
+    Write-Host "Running Test-Function in Test.psm1"
     $piPackageRoot = Join-Path $env:AssemblyPath 'VSEng.PI'
-
+    Write-Host "Looking for VSEng.PI packages in: $piPackageRoot"
     if (Test-Path $piPackageRoot -PathType Container) {
         $piPackage = Get-ChildItem -Path $piPackageRoot -Directory -ErrorAction Stop |
             Where-Object { $_.Name -as [version] } |
             Sort-Object { [version]$_.Name } -Descending |
             Select-Object -First 1
     }
-
-    $binDir = Join-Path $piPackage 'bin'
-
+    Write-Host "Found VSEng.PI package: $($piPackage.FullName)"
+    $binDir = Join-Path $piPackage.FullName 'bin'
+    Write-Host "Looking for bin directory at: $binDir"
     if (-not (Test-Path $binDir -PathType Container)) {
         Write-LogMessage -Message "Bin directory not found at expected location: $binDir" -LogType 'Error'
         throw "Bin directory not found at expected location: $binDir"
