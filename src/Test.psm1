40e273b1f876a08a90865c8807f64bc71b182300
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2026-04-14 11:50:39 +0800

    Test

--- src/Test.psm1
+++ src/Test.psm1
@@ -1,4 +1,18 @@
 function Test-Function() {
-    Write-Host "This is a test function to verify the environment variables"
-    Write-Host "$($env:AssemblyPath)\VSEng.Mailer.dll"
+    $piPackageRoot = Join-Path $env:AssemblyPath 'VSEng.PI'
+    $piPackage = $piPackageRoot
+
+    if (Test-Path $piPackageRoot -PathType Container) {
+        $versionDirectories = Get-ChildItem -Path $piPackageRoot -Directory -ErrorAction Stop |
+            Where-Object { $_.Name -as [version] } |
+            Sort-Object { [version]$_.Name } -Descending
+
+        if ($versionDirectories.Count -gt 0) {
+            $piPackage = $versionDirectories[0].FullName
+        }
+    }
+
+    $binDir = Join-Path $piPackage 'bin'
+
+    Write-Host "Looking for VSEng.Mailer.dll in: $binDir"
 }
