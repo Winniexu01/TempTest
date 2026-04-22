8bb247157648ec9ec80134b0632c728ba233def9
Wenwen Xu (Centific Technologies Inc) <v-wexu@microsoft.com>
2026-04-22 14:08:05 +0800

    Message level

--- src/PackageLoader/PackageLoader/Program.cs
+++ src/PackageLoader/PackageLoader/Program.cs
@@ -10,15 +10,11 @@ namespace PackageLoader
     {
         static void Main(string[] args)
         {
-            var env = Environment.GetEnvironmentVariable("KEYWORDS");
-            Console.WriteLine($"KEYWORDS: {env}");
-
-            var debug = Environment.GetEnvironmentVariable("DEBUG");
-            Console.WriteLine($"DEBUG: {debug}");
-            Console.WriteLine(debug.GetType());
-            var isDebug = bool.TryParse(debug, out var value) ? value : false;
-            Console.WriteLine($"Is Debug Mode: {isDebug}");
-            Console.WriteLine(isDebug.GetType());
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("[DEBUG] This is a debug message.");
+            Console.WriteLine("[INFO] This is an info message.");
+            Console.WriteLine("[ERROR] This is an error message.");
+            Console.WriteLine("[WARNING] This is a warning message.");
         }
     }
 }
